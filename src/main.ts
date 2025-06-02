455e8dbc17c7fa4915030a316fec7fd86d8d9e4c
Doan Quoc Cuong <cuongdq.payment@gmail.com>
2025-06-02 21:26:21 +0700

    add vercel.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { VercelRequest, VercelResponse } from '@vercel/node'; // Cần cài đặt @vercel/node
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+// NestJS cần được bọc trong một hàm serverless
+let app: INestApplication;
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+    if (!app) {
+        app = await NestFactory.create(AppModule, { bodyParser: false });
+        app.enableCors(); // Tùy chọn: Bật CORS
+        await app.init();
+    }
+    // Đây là cách NestJS sẽ xử lý request trong môi trường serverless của Vercel
+    // Import @vercel/node để sử dụng createVercelLambda
+    // Cần cài đặt: npm install @vercel/node
+    const server = app.getHttpAdapter().getInstance();
+    await server(req, res);
 }
-bootstrap();
